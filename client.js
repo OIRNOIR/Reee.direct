ec5b4a482bdd4c29e6fa5d7cee35d3540c0548d3
OIRNOIR <oirnoir@outlook.com>
2022-06-21 19:58:21 -0700

    never gonna give you up

--- client.js
+++ client.js
@@ -4,7 +4,8 @@ window.addEventListener('click', () => {
 	if (complete) return;
 	complete = true;
 	document.getElementById("content").style.display = "none";
-	document.getElementById("stream-container").style.display = "block";
+	document.getElementById("stream-container").style.display = "initial";
+	document.getElementById("ol").style.display = "initial";
 	document.body.classList.add("blackbg");
 	document.getElementById("title").innerText = atob("R0VUIFJJQ0tST0xMJ0Q=");
 	const element = document.getElementById("stream-player_html5_api");
