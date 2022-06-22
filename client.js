4c35ce0d034196936f21701e54dc165d916995b0
OIRNOIR <oirnoir@outlook.com>
2022-06-21 20:26:35 -0700

    Production

--- client.js
+++ client.js
@@ -3,15 +3,21 @@ let complete = false;
 window.addEventListener('click', () => {
 	if (complete) return;
 	complete = true;
-	document.getElementById("content").style.display = "none";
-	document.getElementById("stream-container").style.display = "initial";
+	try {
+		document.getElementById("content").style.display = "none";
+	} catch { }
+	try {
+		document.getElementById("stream-container").style.display = "initial";
+	} catch { }
 	try {
 		document.getElementById("ol").style.display = "initial";
-	} catch {
-
-	}
-	document.body.classList.add("blackbg");
-	document.getElementById("title").innerText = atob("R0VUIFJJQ0tST0xMJ0Q=");
+	} catch { }
+	try {
+		document.body.classList.add("blackbg");
+	} catch { }
+	try {
+		document.getElementById("title").innerText = atob("R0VUIFJJQ0tST0xMJ0Q=");
+	} catch { }
 	const stream = document.getElementById("stream-player_html5_api");
 	stream.play();
 	let streamLoopCount = 0;
