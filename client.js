11367bea5aec7c2cec678b4461f1c1ade458501e
OIRNOIR <oirnoir@outlook.com>
2022-08-06 11:06:21 -0500

    Remove unnececary comment

--- client.js
+++ client.js
@@ -64,10 +64,5 @@ window.addEventListener('DOMContentLoaded', async () => {
 	player = videojs("stream-player");
 	if (Math.floor(Math.random() * 20) == 0 || window.location.hostname.startsWith("2")) {
 		player.src("/assets/stream2/master.m3u8");
-		/*if (document.getElementById("stream-player_html5_api") == null) {
-			document.getElementById("stream-source").setAttribute("src", "/assets/stream2/master.m3u8");
-		} else {
-			document.getElementById("stream-player_html5_api").setAttribute("src", "/assets/stream2/master.m3u8");
-		}*/
 	}
 });
