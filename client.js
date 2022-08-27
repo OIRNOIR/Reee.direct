e76e3c85ef9adceb2e0222cf4f698b0e23704f01
OIRNOIR <oirnoir@outlook.com>
2022-08-26 22:45:38 -0700

    Fuck videojs we using hls now

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 let complete = false;
 let playing = false;
 let plistenerAdded = false;
+let hls = null;
+let completeDate = Infinity;
 window.addEventListener('click', start);
 function start() {
 	if (complete) return;
-	complete = true;
 	try {
 		document.getElementById("content-container").style.display = "none";
 	} catch { }
@@ -20,7 +21,15 @@ function start() {
 	try {
 		document.getElementById("title").innerText = atob("R0VUIFJJQ0tST0xMJ0Q=");
 	} catch { }
+	if (completeDate + 1000 > Date.now()) {
+		setTimeout(() => {
+			return start();
+		}, (completeDate == Infinity ? 1000 : completeDate - (Date.now() - 1000)));
+		return;
+	}
+	complete = true;
 	const stream = document.getElementById("stream-player");
+	stream.removeAttribute("autoplay");
 	stream.play();
 	playing = true;
 	let streamLoopCount = 0;
@@ -59,19 +68,29 @@ var observer = new MutationObserver(() => {
 observer.observe(document.body, {childList: true, subtree: true});
 
 window.addEventListener('DOMContentLoaded', async () => {
-	let src = "https://assets.shortlnkto.com/stream/master.m3u8";
-	if (Math.floor(Math.random() * 20) == 0) {
-		src = "https://assets.shortlnkto.com/stream2/master.m3u8";
+	if (Hls.isSupported()) {
+		let src = "https://assets.shortlnkto.com/stream/master.m3u8";
+		if (Math.floor(Math.random() * 20) == 0) {
+			src = "https://assets.shortlnkto.com/stream2/master.m3u8";
+		}
+		const vidElement = document.createElement("video")
+		vidElement.setAttribute("id", "stream-player");
+		vidElement.setAttribute("poster", "https://assets.shortlnkto.com/BlackBG.png");
+		vidElement.setAttribute("crossorigin", "anonymous");
+		vidElement.setAttribute("preload", "auto");
+		vidElement.setAttribute("autoplay", "");
+		document.getElementById("stream-container").appendChild(vidElement);
+		hls = new Hls();
+		hls.attachMedia(vidElement);
+		hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+      console.log('video and hls.js are now bound together!');
+      hls.loadSource(src);
+      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+        console.log(
+          'manifest loaded, found ' + data.levels.length + ' quality level'
+        );
+      });
+			completeDate = Date.now();
+    });
 	}
-	const vidElement = document.createElement("video")
-	vidElement.setAttribute("id", "stream-player");
-	vidElement.setAttribute("poster", "https://assets.shortlnkto.com/BlackBG.png");
-	vidElement.setAttribute("crossorigin", "anonymous");
-	vidElement.setAttribute("preload", "auto");
-	vidElement.setAttribute("autoplay", "");
-	const srcElement = document.createElement("source");
-	srcElement.setAttribute("src", src);
-	srcElement.setAttribute("id", "stream-source");
-	vidElement.appendChild(srcElement);
-	document.getElementById("stream-container").appendChild(vidElement);
 });
