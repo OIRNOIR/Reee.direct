7efbcda27b243ef170cdfe9e982477aa9bd7c304
OIRNOIR <oirnoir@outlook.com>
2022-11-29 17:42:45 -0800

    Remove some redundant logging

--- client.js
+++ client.js
@@ -97,13 +97,7 @@ window.addEventListener('DOMContentLoaded', async () => {
 		hls = new Hls();
 		hls.attachMedia(vidElement);
 		hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-      console.log('video and hls.js are now bound together!');
       hls.loadSource(src);
-      hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-        console.log(
-          'manifest loaded, found ' + data.levels.length + ' quality level'
-        );
-      });
 			completeDate = Date.now();
     });
 	}
