0bfbd2733e0fb0feb40a353a2e5533ff32b63bae
OIRNOIR <oirnoir@outlook.com>
2022-12-09 08:21:06 -0800

    adsfasdfasdf

--- client.js
+++ client.js
@@ -50,6 +50,11 @@ function start() {
 	});
 }
 
+function supportsHLS() {
+  var video = document.createElement('video');
+  return Boolean(video.canPlayType('application/vnd.apple.mpegURL') || video.canPlayType('audio/mpegurl'))
+}
+
 let ol = document.getElementById("ol");
 var observer = new MutationObserver(() => {
 	if (!playing && !plistenerAdded && document.getElementById("stream-player") != undefined) {
@@ -81,10 +86,19 @@ window.addEventListener('DOMContentLoaded', async () => {
 	vidElement.setAttribute("crossorigin", "anonymous");
 	vidElement.setAttribute("autoplay", "");
 	document.getElementById("stream-container").appendChild(vidElement);
-	hls = new Hls();
-	hls.attachMedia(vidElement);
-	hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-		hls.loadSource(src);
+	if (supportsHLS() && navigator.userAgent.toLowerCase().includes("iphone")) {
+		const srcElement = document.createElement("source");
+		srcElement.setAttribute("src", src);
+		srcElement.setAttribute("id", "stream-source");
+		vidElement.setAttribute("preload", "auto");
+		vidElement.appendChild(srcElement);
 		completeDate = Date.now();
-	});
+	} else if (Hls.isSupported()) {
+		hls = new Hls();
+		hls.attachMedia(vidElement);
+		hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+      hls.loadSource(src);
+			completeDate = Date.now();
+    });
+	}
 });
